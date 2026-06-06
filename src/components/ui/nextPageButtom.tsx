9b56551b89ce4f8be4c3332c0d87db35083910f0
mohamed saad <mohamedsaad5411@gmail.com>
2026-06-06 09:04:51 +0300

    sec commit

--- src/components/ui/nextPageButtom.tsx
+++ src/components/ui/nextPageButtom.tsx
@@ -3,12 +3,14 @@ import { addToCart } from '@/store/slices/cartSlice'
 import toast from 'react-hot-toast'
 import { useDispatch } from 'react-redux'
 import { Product } from '@/types/product'
+import { useCartUser } from '@/hooks/useCartUser'
 
 export default function nextPageButtom({ product }: { product: Product }) {
     const dispatch = useDispatch()
+    const { userId } = useCartUser()
 
     function handleAddToCart() {
-        dispatch(addToCart({ ...product, quantity: 1 }))
+        dispatch(addToCart({ userId, product: { ...product, quantity: 1 } }))
         toast.success("Item added to cart")
     }
 
